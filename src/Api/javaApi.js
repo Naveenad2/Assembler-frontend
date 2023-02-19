cf9a2bf940ebb11f1de694016149b7d7392a710a
Naveenwebhost <na1589970@gmail.com>
2023-02-19 16:32:02 +0530

    first commit

--- src/Api/javaApi.js
+++ src/Api/javaApi.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_URL = "http://assemblerbackend-env.eba-kianmfyy.us-west-2.elasticbeanstalk.com/api/run";
-const API_URL_GET = "http://assemblerbackend-env.eba-kianmfyy.us-west-2.elasticbeanstalk.com/api/getAdd";
-const API_URL_SET_MEMMORY_VALUE = "http://assemblerbackend-env.eba-kianmfyy.us-west-2.elasticbeanstalk.com/api/setMemmoryValue";
-const API_URL_CLEAR_MEMMORY = "http://assemblerbackend-env.eba-kianmfyy.us-west-2.elasticbeanstalk.com/api/ClearAll";
-const API_URL_SHOW_ALL_PROJECTS = "http://assemblerbackend-env.eba-kianmfyy.us-west-2.elasticbeanstalk.com/api/GetAllProject";
-const API_URL_SAVE_PROJECT = "http://assemblerbackend-env.eba-kianmfyy.us-west-2.elasticbeanstalk.com/api/SaveCode";
-const API_URL_GET_CODE_WITH_ID = "http://assemblerbackend-env.eba-kianmfyy.us-west-2.elasticbeanstalk.com/api/getcodewithid/";
+const API_URL = "http://assembler-env.eba-y3mfpnv2.us-west-2.elasticbeanstalk.com//api/run";
+const API_URL_GET = "http://assembler-env.eba-y3mfpnv2.us-west-2.elasticbeanstalk.com//api/getAdd";
+const API_URL_SET_MEMMORY_VALUE = "http://assembler-env.eba-y3mfpnv2.us-west-2.elasticbeanstalk.com//api/setMemmoryValue";
+const API_URL_CLEAR_MEMMORY = "http://assembler-env.eba-y3mfpnv2.us-west-2.elasticbeanstalk.com//api/ClearAll";
+const API_URL_SHOW_ALL_PROJECTS = "http://assembler-env.eba-y3mfpnv2.us-west-2.elasticbeanstalk.com//api/GetAllProject";
+const API_URL_SAVE_PROJECT = "http://assembler-env.eba-y3mfpnv2.us-west-2.elasticbeanstalk.com//api/SaveCode";
+const API_URL_GET_CODE_WITH_ID = "http://assembler-env.eba-y3mfpnv2.us-west-2.elasticbeanstalk.com//api/getcodewithid/";
 
 class JavaApi{
 
